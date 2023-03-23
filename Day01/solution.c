fcc133abb61f78b7b0c7a726add72a3c47efb9fc
WafaBenyettou <benwafa951@gmail.com>
2023-03-23 10:31:00 +0100

    challenge 01 - setup

---
+++ Day01/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+
+   
+   printf("Hello Guys!");
+
+
+   return 0; 
+}

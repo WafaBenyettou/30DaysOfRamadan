324fa88759bc33b35382914c36390896242905b3
Amine-Dz <112625154+DanaAmine@users.noreply.github.com>
2023-03-23 20:16:07 +0100

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,11 @@
+/******************************* ITC **************************/
+let T = parseInt(ReadLine());
+
+for (var i = 0; i < T; i++) {
+  let x = ReadLine().split(" ");
+  n = parseInt(x[0]);
+  m = parseInt(x[1]);
+  console.log(n * 5 + m * 7);
+}
+
+/******************************* ITC **************************/

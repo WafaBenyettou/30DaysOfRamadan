e14445c6efd43aa48e04c7adb122ad7a7d3a0ea6
menialoubna <77592692+menialoubna@users.noreply.github.com>
2023-03-25 17:56:20 +0100

    Update solution.c

--- Day03/solution.c
+++ Day03/solution.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+k = input("how many income ")
+for i in range (1,int(k)):
+    X = input("enter your income ")
+    Y = input("what's The amount above which you will have to pay taxes ")
+    T1 = int(X) - int(Y)
+    T2 =int(X) - int(Y) + T1
 
-int main(){
+    if int(X) > int(Y):
 
-   printf("Pay your taxes kids ;) !");
-   return 0; 
-}
+        print("you need to invest at least " + str(T1))
+    else:
+        print("you don't need to invest")
+
+print("the amount invested after " + str(k) + "incomes , is " + str(T2))

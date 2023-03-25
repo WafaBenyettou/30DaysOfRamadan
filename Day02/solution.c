7282f2f88f82274c19c576d23ec6df653a50df92
WafaBenyettou <benwafa951@gmail.com>
2023-03-25 16:45:11 +0100

    Solution Day 02

--- Day02/solution.c
+++ Day02/solution.c
@@ -6,9 +6,21 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
 int main(){
-
-
-   printf("Green light! Red light!");
-   return 0; 
+    int t,a,b;
+    scanf("%d",&t);
+    while (t--){
+    scanf("%d %d",&a,&b);
+    int i=0,count=0;
+    while (i<a){
+        int c;
+        scanf("%d",&c);
+        if (c>b) count+=1;
+        i+=1;
+    }
+    printf("%d\n",count);
+    }
+    return 0;
+    
 }

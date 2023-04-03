4c927056a36a4396e7b85f999288e1458e632d60
WafaBenyettou <benwafa951@gmail.com>
2023-04-03 23:20:50 +0200

    init day 12

---
+++ Day12/solution.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+   printf("Hello !!");
+   return 0; 
+}

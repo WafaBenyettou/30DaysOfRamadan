a50de093b12348382aa117f69ad4f408a65542c6
WafaBenyettou <benwafa951@gmail.com>
2023-04-05 21:45:11 +0100

    The solution Day 13

--- Day13/solution.c
+++ Day13/solution.c
@@ -1,15 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-int main(){
-  
-  
-  printf("NWAYS or NO WAYS ?!");
-  return 0; 
-
+#include<bits/stdc++.h>
+#define pb push_back
+#define int long long int
+#define vec vector<int>
+#define REP(i,a,b) for(i=a;i<b;i++)
+using namespace std;
+int mod=1e9+7;
+int fact[3000001];
+void factorial()
+{
+  fact[1]=1,fact[0]=1;
+  for(int i=2;i<3000001;i++)
+    fact[i]=((i%mod)*(fact[i-1]%mod))%mod;
+}
+int inverse(int n)
+{
+  n%=mod;
+  int res=1,b=mod-2;
+  while(b>0)
+  {
+    if(b&1)
+        res=((res%mod)*(n%mod))%mod;
+    n=((n%mod)*(n%mod))%mod;
+    b>>=1;
+  }
+  return res%mod;
+}
+main()
+{
+  factorial();
+  int t;
+  cin>>t;
+  while(t--)
+  {
+    int n,k;
+    cin>>n>>k;
+    cout<<((2*((fact[n+k+1]%mod)*((inverse(fact[k+2])%mod)*(inverse(fact[n-1])%mod)%mod)))%mod +mod -n)%mod<<"\n";
+  }
 }

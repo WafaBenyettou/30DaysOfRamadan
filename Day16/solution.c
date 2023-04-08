1f131d263455d900b52b3e346f32ccaab85dc536
WafaBenyettou <benwafa951@gmail.com>
2023-04-08 23:40:58 +0100

    The solution Day 16

--- Day16/solution.c
+++ Day16/solution.c
@@ -10,7 +10,7 @@
      int mod=998244353;
      int32_t main()
      {
-       ios_base::sync_with_stdio(false);
+      ios_base::sync_with_stdio(false);
      cin.tie(NULL);
        cout.tie(NULL);
        int tt=1;

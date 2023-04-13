99065565224da086ee115f72dfb52ea7768d0cd9
WafaBenyettou <benwafa951@gmail.com>
2023-04-13 04:03:11 +0100

    The solution Day 20

--- Day20/solution.c
+++ Day20/solution.c
@@ -1,14 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <bits/stdc++.h>
+#define lli long long int
+#define endl "\n"
+using namespace std;
 
 int main(){
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,k,l;
+		cin>>n>>k>>l;
+		if(n > (k*l))
+			cout<<-1<<endl;
+		else if(k == 1)
+		{
+			if(n == 1)
+				cout<<1;
+			else
+				cout<<-1;
+		}
+		else
+		{
+			int i = 1;
+			while(n--)
+			{
+				cout<<i<<" ";
+				i++;
+				if(i > k)
+					i = 1;
+			}
+		}
+		cout<<endl;
 
+	}
 
-   printf("20 DAYS - Bowling Strategy");
-   return 0; 
+	return 0;
 }

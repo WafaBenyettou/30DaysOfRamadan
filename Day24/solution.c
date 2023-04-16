313b10d354695d379cf60e3eea5017aad2db266f
WafaBenyettou <benwafa951@gmail.com>
2023-04-16 05:16:41 +0100

    init Day 24

---
+++ Day24/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(){
+
+
+   printf("Hello day 24!");
+   return 0; 
+}

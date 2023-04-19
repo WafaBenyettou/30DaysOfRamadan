3669c83bc4df77ecbb5cb6646e51a891ff3ac339
WafaBenyettou <benwafa951@gmail.com>
2023-04-19 19:00:37 +0100

    init Day 28

--- Day28/solution.c
+++ Day28/solution.c
@@ -8,17 +8,8 @@
 
 int main(){
 
-   
-     int t,res, N,M;
-    
-    scanf("%d", &t);
-    
-    while(t--){
-        scanf("%d%d", &N,&M);
-        res= N*5+7*M;
-        printf("%d\n",res);
-    }
 
+   printf("Daaaaaaaayyyyyyyyyyyyy 28");
 
    return 0; 
 }

755352ff5f1f6abebd4ff0e69aef24a25bb15950
WafaBenyettou <benwafa951@gmail.com>
2023-04-19 14:27:38 +0100

    The solution Day 27

--- Day27/solution.c
+++ Day27/solution.c
@@ -1,13 +1,50 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-int main(){
 
-   printf("Longest Increasing Subsequence (LIS)");
-   return 0; 
+int _lis(int arr[], int n, int* max_ref)
+{
+	// Base case
+	if (n == 1)
+		return 1;
+
+
+	int res, max_ending_here = 1;
+
+	for (int i = 1; i < n; i++) {
+		res = _lis(arr, i, max_ref);
+		if (arr[i - 1] < arr[n - 1]
+			&& res + 1 > max_ending_here)
+			max_ending_here = res + 1;
+	}
+
+	if (*max_ref < max_ending_here)
+		*max_ref = max_ending_here;
+
+
+	return max_ending_here;
+}
+
+
+int lis(int arr[], int n)
+{
+
+	int max = 1;
+
+
+	_lis(arr, n, &max);
+
+
+	return max;
+}
+
+int main()
+{
+	int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	// Function call
+	printf("Length of lis is %d", lis(arr, n));
+	return 0;
 }
